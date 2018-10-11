c9e25248c0e034514fc3c025a64d9901ae694e24
blackcathacker <douglas.pace@gmail.com>
2018-10-11 14:30:50 -0700

    testing version

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/demo/healthCheck', (req, res) => {
     res.send({
-      version:"DEMO APP HEALTH CHECK",
+      version:"DEMO APP HEALTH CHECK v1",
       timeLeft
     })
 })
@@ -12,7 +12,7 @@ app.get('/demo/healthCheck', (req, res) => {
 app.use(homepage)
 
 function homepage(req, res) {
-  res.status(200).send('Demo is up and running')
+  res.status(200).send('Demo is up and running on version 1')
 }
 
 const port = process.env.PORT || 80
